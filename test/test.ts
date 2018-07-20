d9997406a15f93411513142eeef1e6ba4ba6070c
marcin wykret <marcin.wykret@capsilon.com>
2018-07-20 15:50:54 +0200

    init

---
+++ test/test.ts
@@ -0,0 +1,13 @@
+
+
+import * as chai from "chai";
+
+const assert=chai.assert;
+
+describe('Array', function() {
+    describe('#indexOf()', function() {
+        it('should return -1 when the value is not present', function() {
+            assert.equal([1,2,3].indexOf(4), -1);
+        });
+    });
+});
